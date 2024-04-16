c21394c197d00f6f97141bd42a75a3fa9777cc11
Asif Hirani <asifhirani@hotmail.com>
2024-04-16 11:09:39 +0000

    completed Arrow function lesson and challenge

--- arrow-functions.js
+++ arrow-functions.js
@@ -13,6 +13,7 @@ let sum1 = addTwoNumbers1(3, 5);
 console.log(sum1);
 
 // Arrow Function With Parameters
+
 const addTwoNumbers2 = (a, b) => {
   return a + b;
 };
@@ -20,9 +21,27 @@ let sum2 = addTwoNumbers2(3, 5);
 console.log(sum2);
 
 // Single Line Arrow Function With Parameters
+
 const addTwoNumbers3 = (a, b) => a + b;
 let sum3 = addTwoNumbers3(6, 4);
 console.log(sum3);
+
 // Implicit Returns
 
+const saySomething = (message) => console.log(message);
+saySomething("Hello, World!");
+
+// Empty set of parenthese to tell JavaScript that we are declaring the function
+
+const sayHello = () => console.log("Hello");
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => `<p>
+      
+This is a multiline code!
+  
+</p>
+`;
+console.log(returnMultipleLines());
