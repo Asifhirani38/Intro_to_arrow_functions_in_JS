d3d14ae39592a534a69f768ef479869fe6eafd56
Asif Hirani <asifhirani@hotmail.com>
2024-04-16 09:38:51 +0000

    adding local changes before the pull request

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,14 +1,28 @@
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node arrow-functions.js in the terminal
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers1(a, b) {
+  //Code block
+  return a + b;
+}
+
+let sum1 = addTwoNumbers1(3, 5);
+console.log(sum1);
 
 // Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => {
+  return a + b;
+};
+let sum2 = addTwoNumbers2(3, 5);
+console.log(sum2);
 
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers3 = (a, b) => a + b;
+let sum3 = addTwoNumbers3(6, 4);
+console.log(sum3);
 // Implicit Returns
 
 // Returning Multiple Lines
